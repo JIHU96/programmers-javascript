0a1dd4509f41cbf3cfecbbf9385a8c40bb862f0a
Jihun Noh <87410294+JIHU96@users.noreply.github.com>
2024-12-18 23:06:36 +0900

    [level 2] Title: 전화번호 목록, Time: 132.32 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,8 @@
 function solution(phone_book) {
+    // 문자열 정렬을 위해 compareFunction 제공 X
     const sortedBook = phone_book.sort()
-    
+    // some 메서드로 하나라도 접두어가 있는지 검사
     const answer = sortedBook.some((value, index, self) => self[index+1]?.indexOf(value) === 0)
-
+    
     return !answer;
 }
