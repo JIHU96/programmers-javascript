540f65f792d1ddd86e5704c3e773d782ca740ac6
Jihun Noh <87410294+JIHU96@users.noreply.github.com>
2025-01-24 04:16:20 +0900

    [level 0] Title: 옹알이 (1), Time: 3.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,7 +1,23 @@
 function solution(babbling) {
-    const reg = /^(aya|ye|woo|ma)+$/;
+  const reg = /^(aya|ye|woo|ma)+$/;
 
   const res = babbling.reduce((count, babu) => (reg.test(babu) ? count + 1 : count), 0);
-
-  return res;
+  
+  const possible = ["aya", "ye", "woo", "ma" ]
+  
+  let arr = []
+  
+   for(let b of babbling) {
+        for(const p of possible) {
+             b = b.replace(p, "_")
+   
+        }
+    arr.push(b)
+   }
+    
+    const test = arr.map(str => str.split('').every(char => char === '_'))
+    const count = test.filter(v => v).length
+    console.log(count)
+    
+  return count;
 }
